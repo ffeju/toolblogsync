6380887215399061c4098f5f445b74044373a91e
fengjun <fengjun@gzgxinfo.com>
2020-11-19 01:05:50 +0800

    init project

---
+++ src/main/java/org/kcp/toolblogsync/backserver/BackserverApplication.java
@@ -0,0 +1,13 @@
+package org.kcp.toolblogsync.backserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kcp/toolblogsync/backserver/BackserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kcp.toolblogsync.backserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
